8c78a6c1d54a12a1462c173681fc99343f87cb5d
karthi9772 <karthi.g9772@gmail.com>
2024-10-06 15:54:17 +0530

    snippet modification

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@ export const LANGUAGE_VERSIONS = {
 
 export const CODE_SNIPPETS = {
   javascript: `console.log("Hello, World!");\n`,
-  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
+  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Name" });\n`,
   python: `print("Hello World")`,
   java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
   csharp:
